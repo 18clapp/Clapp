87511141fb0622e2fa10b5739b0be1f5f93f2596
Afonso <afonso.martinho@hotmail.com>
2018-04-07 19:28:56 +0100

    Created Branch ListEvents

--- app/src/main/java/com/project/clapp/AllEvents.java
+++ app/src/main/java/com/project/clapp/AllEvents.java
@@ -41,7 +41,7 @@ public class AllEvents extends Fragment {
         // Required empty public constructor
     }
 
-
+    //
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
